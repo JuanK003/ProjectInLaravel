62e77b3c3811ccceb000d8fd1ac4ba9b6d0c6e13
JuanK003 <juankdev003@gmail.com>
2022-08-24 12:52:25 -0600

    add styles on form

--- resources/views/dashboard/post/create.blade.php
+++ resources/views/dashboard/post/create.blade.php
@@ -4,24 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('/form.css')}}">
     <title>Mi primer CRUD</title>
 </head>
 <body>
-    <h1>Ingreso de Post</h1><br><br>
+    <h1 class="Title"><a href="">Ingreso de Post</a></h1><br><br>
 
-    <form action="{{route('post.store')}}" method='post'>
+    <form action="{{route('post.store')}}" method='post' class="form">
         @csrf
         <label for="">Titulo</label>
-        <input type="text" name="title">
+        <input type="text" name="title" placeholder="Escribe el Título">
 
         <label for="">URL Corta</label>
-        <input type="text" name="slug">
+        <input type="text" name="slug" placeholder="Colaca la URL">
 
         <label for="">Descripción</label>
-        <textarea name="description"></textarea>
+        <textarea name="description" placeholder="Coloca descripción"></textarea>
 
         <label for="">Contenido</label>
-        <textarea name="content"></textarea>
+        <textarea name="content" placeholder="Colaca el contenido"></textarea>
 
         <button type='submit'>Enviar</button>
     </form>

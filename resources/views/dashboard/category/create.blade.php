2292e3281c30efa3f83442a2918c575a08aded3b
JuanK003 <juankdev003@gmail.com>
2022-08-30 21:34:59 -0600

    Add the view Categories

--- resources/views/dashboard/category/create.blade.php
+++ resources/views/dashboard/category/create.blade.php
@@ -2,57 +2,39 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('/post.css')}}">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('/categories.css')}}">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-    <title>Mi primer CRUD</title>
+    <title>Categories</title>
 </head>
 <body>
-    <h1 class="Title"><a href="">Ingreso de Post</a></h1><br><br>
-    <form action="{{route('post.store')}}" method='post' class="form">
-       {{--  @if ($errors->any())
-            @foreach ($errors->all() as $error)
-                <div class="alert alert-danger" role="alert">
-                    {{$error}}
-                </div>
-            @endforeach
-        @endif --}}
+    <form action="{{route('category.store')}}" method='category' class="form-register">
         @csrf
         @if (session('status'))
             <div class="alert alert-success" role="alert">
                 {{session('status')}}
             </div>
         @endif
-        <label for="">Titulo</label>
-        <input type="text" name="title" placeholder="Escribe el Título">
+        <h4>Add Categories</h4>
+        <input class="controls" type="text" name="title" id="title" placeholder="Title">
         @error("title")
             <small class="text-danger">
                 {{$message}}
                 <br>
             </small>
         @enderror
-
-        <label for="">URL Corta</label>
-        <input type="text" name="slug" placeholder="Colaca la URL">
+        <input class="controls" type="text" name="slug" id="slug" placeholder="Slug">
         @error("slug")
             <small class="text-danger">
                 {{$message}}
                 <br>
             </small>
         @enderror
-        <label for="">Contenido</label>
-        <textarea name="content" placeholder="Colaca el contenido"></textarea>
-        @error("content")
-            <small class="text-danger">
-                {{$message}}
-            </small>
-        @enderror
-
-        <button type='submit'>Enviar</button>
-    </form>
+        <button type="submit" class="botons">Save</button>
+      </form>
 </body>
 </html>
